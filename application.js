06c8a3a6238f7952022df9c279a80de37e048ad8
Deb Shaw <debashishshaw@gmail.com>
2020-10-20 09:24:09 +0200

    Question list working

--- application.js
+++ application.js
@@ -1,19 +1,38 @@
 $(document).ready(function() {
+  //Run random number generator
   pickRandomNumber();
+
+  //Initiate 10 second countdown function on input field keydown
+  $("#answer-field").keydown(function() {
+    startTimer(10);
+  })
+
+  //Check provided answer
+  $("#answer-field").keyup(function() {
+    if ($("#answer-field").val().length === rightAnswer.toString().length) {
+      checkAnswer();
+    }
+  })
 });
+
+//Random number generator function
+var rightAnswer;
 
 var pickRandomNumber = function() {
   var firstNumber = Math.floor((Math.random() * 10) + 1);
   var secondNumber = Math.floor((Math.random() * 10) + 1);
-  var rightAnswer = firstNumber + secondNumber;
-  $('.question-field').append($('<p>' + firstNumber + '&nbsp;' + '+' + '&nbsp;' + secondNumber + ' = ' + '</p>'));
+  rightAnswer = firstNumber + secondNumber;
+  $('.question-field').append($('<p>' + firstNumber + '&nbsp;' + '+' + '&nbsp;' + secondNumber + ' = ' + '</p>'));;
 }
 
+//Countdown function
 var timer = null;
-var countDown;
-var startTime;
+var timeLimit;
 
 var startTimer = function(timeLimit) {
+  var countDown;
+  var startTime;
+
   if (timer === null) {
     $("#timer").text(timeLimit);
     startTime = Date.now();
@@ -33,3 +52,19 @@ var stopTimer = function() {
   clearInterval(timer);
   timer = null;
 };
+
+//Read input field and determine right answer
+var checkAnswer = function() {
+  if ($("#answer-field").val().toString() === rightAnswer.toString()) {
+    console.log("hooray!");
+    timeLimit++;
+    $('.question-field').empty();
+    $('#answer-field').val('');
+    pickRandomNumber();
+  }
+  else {
+    console.log($("#answer-field").val());
+    console.log(rightAnswer);
+    console.log("oops");
+  }
+}
